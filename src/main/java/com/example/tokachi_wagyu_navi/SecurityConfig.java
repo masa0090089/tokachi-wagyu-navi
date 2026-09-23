f884f60b26a0deed91fc70e6d6dfd294fa54bdf9
masa0090089 <coca1win@gmail.com>
2026-09-23 21:31:06 +0900

    Update SecurityConfig.java

--- src/main/java/com/example/tokachi_wagyu_navi/SecurityConfig.java
+++ src/main/java/com/example/tokachi_wagyu_navi/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.example.tokachi_wagyu_navi;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,12 +13,10 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable()) // CSRF保護を無効化
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(HttpMethod.GET, "/shops").permitAll() // 一覧取得は誰でもOK
-                        .anyRequest().authenticated() // それ以外は認証が必要
-                )
-                .httpBasic(httpBasic -> {}); // Basic認証を有効化
+                        .anyRequest().permitAll()
+                );
 
         return http.build();
     }
